819d1d7eff2efe1ae36c90e5088c5a479966b805
Pratik <pratikofficial200@gmail.com>
2025-04-14 02:29:28 +0530

    Updating Vite.config.js

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: process.env.PORT || 5173,
+    host: '0.0.0.0'
+  },
   plugins: [react()],
 })
+
+
+
