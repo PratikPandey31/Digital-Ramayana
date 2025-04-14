078397b006032d14242e817dc2531205f2065e17
Pratik <pratikofficial200@gmail.com>
2025-04-14 14:13:20 +0530

    Uploading again

--- Server/index.js
+++ Server/index.js
@@ -103,7 +103,7 @@ app.get('/balKand/:id', verifyToken, async (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
