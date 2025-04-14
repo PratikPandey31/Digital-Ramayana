976c6a381975460e6d05ebfee6751dcb858ea6d8
Pratik <pratikofficial200@gmail.com>
2025-04-14 14:45:43 +0530

    Uploading again

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: process.env.PORT || 5173,
+    port:5173,
     allowedHosts: ['digital-ramayana.onrender.com'],
     host: '0.0.0.0'
   },
