a63ba03966384de1324fc673e148f67f308e2277
Pratik <pratikofficial200@gmail.com>
2025-04-14 15:01:28 +0530

    Changing Vite config

--- FrontEnd/vite.config.js
+++ FrontEnd/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port:5173,
+    port: process.env.PORT || 5173,
     allowedHosts: ['digital-ramayana.onrender.com'],
     host: '0.0.0.0'
   },
